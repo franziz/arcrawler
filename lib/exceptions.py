ff4a63376d0096708976af62dd06e2d56b99563d
Frans <franssiswanto@gmail.com>
2016-08-05 20:44:54 +0800

    add configuration exception

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -7,3 +7,13 @@ class ConfigNotFound(Exception):
 		return repr(self.value)
 	#end def
 #end class
+
+class ConfigNotFound(Exception):
+	def __init__(self,value):
+		self.value = value
+	#end def
+
+	def __str__(self):
+		return repr(self.value)
+	#end def
+#end class
