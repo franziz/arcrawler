a30e629128f5086701d2e3fdab1f1c1b05a8dc3d
Frans <franssiswanto@gmail.com>
2016-08-05 20:43:58 +0800

    fix 'parse' parameter implementation

--- lib/network_tools.py
+++ lib/network_tools.py
@@ -16,13 +16,31 @@ class NetworkTools(object):
 		return '{uri.scheme}://{uri.netloc}/'.format(uri=url)
 
 	def parse(self,url=None, parse=True):
+		""" This function will help to get web source code as HTML document.
+			After getting the source code, the function will later decide wheter it will parse to
+			HTML Object or leave it as a string. 
+
+			Parse function will automatically failed if face errors for more than 10 errors. 
+			Even if the parse failed, the function will return a HTML Object.
+			However, the HTML Object will be blank.
+
+			Parameters details:
+			- url   : a url to be parse
+			- parse : if you speciy parse, means parse to html object using html.fromstring function
+
+			Return details:
+			- _html : This variable can be a string or html object. 
+		"""
 		assert url is not None, "url is not defined."
 
 		proxy_is_ok = False
 		_html       = None
-		while not proxy_is_ok:
+		tried  		= 0
+		max_try 	= 10
+		while not proxy_is_ok and tried < max_try:
 			try:
 				assert url is not None, "URL is not defined."
+				tried = tried + 1
 				
 				headers = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
 				if self.use_proxy:
@@ -46,12 +64,13 @@ class NetworkTools(object):
 				proxy_is_ok = False
 			except lxml.etree.XMLSyntaxError:
 				print("Ops! Something wrong. Leave it~")
-				_html       = html.fromstring("<html></html>")
+				_html       = html.fromstring("<html></html>") if parse else "<html></html>"
 				proxy_is_ok = True
 			except:
 				raise
-			#end try
-		#end while
+		# proxy_is_ok == False only when max_tried exceed.
+		if proxy_is_ok == False:
+			_html = html.fromstring("<html></html>") if parse else "<html></html>"
 		return _html
 	#end def
 #end class
