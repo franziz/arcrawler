c906df310b2aa0a04bd8ceb628ef585627f65c6f
Frans <franssiswanto@gmail.com>
2016-11-03 21:49:44 +0700

    move generate_meta

--- mini_apps/generate_meta.py
+++ mini_apps/generate_meta.py
@@ -3,30 +3,32 @@
 
 	In short, this is just crawler metadata for monitoring and other purpose.
 """
+import os
+import sys
+
+TEST_DIR   = os.path.abspath(os.path.dirname(__file__))
+PARENT_DIR = os.path.join(TEST_DIR, '..')
+sys.path.insert(0, PARENT_DIR)
+
 from lib.factory.explorer import ExplorerFactory
 from lib.factory.saver    import SaverFactory
 from lib.network_tools    import NetworkTools
+import inspect
 
 if __name__ == "__main__":
 	explorer = ExplorerFactory.get_explorer(ExplorerFactory.SOURCE_FILES)
 	sources  = explorer.explore()
 
-	for key, source in sources.items():
-		old_domain = NetworkTools.get_domain(source.LINK_TO_CRAWL[0], with_scheme=False)
-		new_domain = old_domain.lower().replace("www.","")
-		domain     = "%s%s" % (new_domain[0].title(), new_domain[1:])
-		print("Changing %s to %s" % (old_domain, domain))
-
+	for key, source in sources.items():		
 		document = {
-			"domain" : domain,
-			  "name" : source.CRAWLER_NAME.title(),
-  			    "db" : {
-			      		  "name" : source.DB_SERVER_NAME,
-			      		  "host" : "220.100.163.132",
-			              "port" : 27017,
-			        "collection" : "data"
-			    }
+			"name" : source.CRAWLER_NAME.title(),
+			  "db" : {
+					    "name" : source.DB_SERVER_NAME,
+					    "host" : "220.100.163.132",
+					    "port" : 27017,
+				  "collection" : "data"
+			  },
+			"type" : source.TYPE
 		}
-		
 		saver = SaverFactory.get_saver(SaverFactory.META)
 		saver.save(document)
