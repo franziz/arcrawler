8cada4a5c13aed82303a63a3aca5b75d48f4f753
Frans <franssiswanto@gmail.com>
2016-11-15 11:09:11 +0700

    point to server

--- lib/logger/__init__.py
+++ lib/logger/__init__.py
@@ -5,7 +5,7 @@ import logging
 
 class Logger(raven.Client):
 	def __init__(self, **kwargs):
-		self.dsn = "http://3ad4e70e205c4eb793061be7527d9d7d:472435681da448a583d219164febf92c@sentry:9000/2"
+		self.dsn = "http://5570134675474d8682b476c6da319812:000f2481edcb4eda870c7d05f0126f3a@sentry:9000/2"
 		raven.Client.__init__(self, self.dsn, auto_log_stacks=True, **kwargs)
 
 		self.handler = SentryHandler(self)
