097bcc0706624b709f2e624ca4475fcc9b2546b3
Frans <franssiswanto@gmail.com>
2016-11-17 16:50:09 +0700

    template changes

--- lib/saver/article.py
+++ lib/saver/article.py
@@ -3,8 +3,9 @@ import pymongo
 import bson.json_util
 
 class ArticleSaver:
-	def __init__(self):
-		pass
+	def __init__(self, **kwargs):
+		self.db_address = kwargs.get("db_address", "mongo:27017")
+		self.db_name    = kwargs.get("db_name", "news_crawler")
 
 	def save(self, article=None):
 		""" Exceptions:
@@ -12,8 +13,8 @@ class ArticleSaver:
 		"""
 		assert article is not None, "article is not defined."
 
-		conn = pymongo.MongoClient("mongodb://220.100.163.132/news_crawler")
-		db   = conn["news_crawler"]
+		conn = pymongo.MongoClient("mongodb://%s/%s" % (self.db_address, self.db_name))
+		db   = conn[self.db_name]
 
 		# Ensuring index		
 		db.data.create_index([("permalink", pymongo.ASCENDING)], unique=True, background=True)
--- lib/saver/blog.py
+++ lib/saver/blog.py
@@ -3,14 +3,18 @@ import pymongo
 import bson.json_util
 
 class BlogSaver:
-	def __init__(self):
-		pass
+	def __init__(self, **kwargs):
+		self.db_address = kwargs.get("db_address", "mongo:27017")
+		self.db_name    = kwargs.get("db_name", "news_crawler")
 
 	def save(self, article=None):
+		""" Exceptions:
+			-  AssertionError
+		"""
 		assert article is not None, "article is not defined."
-		
-		conn = pymongo.MongoClient("mongodb://220.100.163.132/blog_crawler")
-		db   = conn["blog_crawler"]
+
+		conn = pymongo.MongoClient("mongodb://%s/%s" % (self.db_address, self.db_name))
+		db   = conn[self.db_name]
 
 		# Ensuring index		
 		db.data.create_index([("permalink", pymongo.ASCENDING)], unique=True, background=True)
